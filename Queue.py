f4dbe10fa6a8f6d953bf894045bb122fe4649130
David Assouline <david.assouline@hotmail.com>
2022-01-30 18:50:31 -0500

    Queues

---
+++ Queue.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        else:
+            temp = self.first
+            if self.length == 1:
+                self.first = None
+                self.last = None
+            else:
+                self.first = self.first.next
+                temp.next = None
+            self.length -= 1
+            return temp
+
+
+my_queue = Queue(4)
+my_queue.enqueue(2)
+
+

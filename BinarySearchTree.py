a5479031022597e90bec788642cab610682a946d
David Assouline <david.assouline@hotmail.com>
2022-02-01 19:18:18 -0500

    BFS / DFS

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -47,6 +47,58 @@ class BinarySearchTree:
             current_node = current_node.left
         return current_node.value
 
+    def bfs(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+
+    def dfs_pre_order(self):
+        results = []
+
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+        traverse(self.root)
+        return results
+
+    def dfs_post_order(self):
+        results = []
+
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+
+        traverse(self.root)
+        return results
+
+    def dfs_in_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+        traverse(self.root)
+
 
 my_tree = BinarySearchTree()
 my_tree.insert(47)
@@ -56,9 +108,3 @@ my_tree.insert(18)
 my_tree.insert(27)
 my_tree.insert(52)
 my_tree.insert(82)
-
-print(my_tree.contains(27))
-print(my_tree.contains(17))
-
-print(my_tree.min_value_node(my_tree.root))
-print(my_tree.min_value_node(my_tree.root.right))
